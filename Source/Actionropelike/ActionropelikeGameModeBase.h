c03b574fa9903177bb63fd3f7f8761942d7cb362
heimaTest <2368915031@qq.com>
2023-10-08 15:05:04 +0800

    commitment

---
+++ Source/Actionropelike/ActionropelikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionropelikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROPELIKE_API AActionropelikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
